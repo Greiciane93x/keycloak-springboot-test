6cd7a37eb73ad92a6b17720349dd8fb39d24f0b3
Greiciane93x <greiciane.assis@hotmial.com>
2023-09-06 23:40:02 -0300

    first commit

---
+++ src/main/java/com/ane/example/AuthenticationKeycloackApplication.java
@@ -0,0 +1,13 @@
+package com.ane.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationKeycloackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationKeycloackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ane/example/AuthenticationKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ane.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationKeycloackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
